a76f682ca37135a72a02cdb04bd445cf13a02a9e
Civa <civa@cryptodeploy.io>
2021-08-16 18:52:59 +0000

    first commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,64 @@
+import { MenuEntry } from 'zmx-uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'http://dex.zum.finance/#/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: 'http://dex.zum.finance/#/pool',
+      },
+      {
+        label: 'Liquidity V2',
+        href: 'https://pancakeswap.finance/swap#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farm and Stake',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  // {
+  //  label: 'poolss',
+  //  icon: 'PoolIcon',
+  //  href: '/poolss',
+  // },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/ZUMFinance',
+      },
+    ],
+  },
+]
+
+export default config
---
+++ src/config/constants/farms.ts
@@ -0,0 +1,87 @@
+import contracts from './contracts'
+import { FarmConfig, QuoteToken } from './types'
+
+const farms: FarmConfig[] = [
+  {
+    pid: 0,
+    risk: 5,
+    lpSymbol: 'ZMX-BNB LP',
+    lpAddresses: {
+      97: '',
+      56: '0xB8c281fe1b315Fec8928a10057404D9802F5cc45',
+    },
+    tokenSymbol: 'ZMX',
+    tokenAddresses: {
+      97: '',
+      56: '0xf8e031265601419b84840617Cd18b658d29DDc41',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+  {
+    pid: 1,
+    risk: 5,
+    lpSymbol: 'ZUM BNB',
+    lpAddresses: {
+      97: '',
+      56: '0x5eC714a1f3A04Ab04760E97c1642B60e90bCf576',
+    },
+    tokenSymbol: 'ZUM',
+    tokenAddresses: {
+      97: '',
+      56: '0x54c2c07b3af037567269ad6a168d5bd527867b58',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+  {
+    pid: 2,
+    risk: 5,
+    lpSymbol: 'BNB-BUSD LP',
+    lpAddresses: {
+      97: '',
+      56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
+    },
+    tokenSymbol: 'BNB',
+    tokenAddresses: {
+      97: '',
+      56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+    },
+    quoteTokenSymbol: QuoteToken.BUSD,
+    quoteTokenAdresses: contracts.busd,
+  },
+  {
+    pid: 3,
+    risk: 5,
+    lpSymbol: 'ZMX POOL',
+    lpAddresses: {
+      97: '',
+      56: '0xf8e031265601419b84840617Cd18b658d29DDc41',
+    },
+    tokenSymbol: 'ZMX',
+    tokenAddresses: {
+      97: '0xf8e031265601419b84840617Cd18b658d29DDc41',
+      56: '0x00375ff8ede1c7ca76c1821f804dec52c6d1f627',
+    },
+    quoteTokenSymbol: QuoteToken.CAKE,
+    quoteTokenAdresses: contracts.cake,
+  },
+  {
+    pid: 4,
+    risk: 5,
+    lpSymbol: 'ZUM POOL',
+    lpAddresses: {
+      97: '',
+      56: '0x54C2c07b3Af037567269Ad6A168D5bD527867b58',
+    },
+    tokenSymbol: 'ZUM',
+    tokenAddresses: {
+      97: '',
+      56: '0x00375ff8ede1c7ca76c1821f804dec52c6d1f627',
+    },
+    quoteTokenSymbol: QuoteToken.CAKE,
+    quoteTokenAdresses: contracts.cake,
+  },
+]
+
+export default farms
