0591201e7580d9762e17ad1fa6f5c720f587dfc0
Civa <44102586+civa@users.noreply.github.com>
2021-08-21 14:57:38 +0100

    update dex links

--- src/views/Farms/components/FarmCard/DetailsSection.tsx
+++ src/views/Farms/components/FarmCard/DetailsSection.tsx
@@ -55,8 +55,8 @@ const DetailsSection: React.FC<ExpandableSectionProps> = ({
         <StyledLinkExternal
           href={
             isTokenOnly
-              ? `https://exchange.goosedefi.com/#/swap/${tokenAddresses[process.env.REACT_APP_CHAIN_ID]}`
-              : `https://exchange.goosedefi.com/#/add/${liquidityUrlPathParts}`
+              ? `https://dex.zum.finance/#/swap/${tokenAddresses[process.env.REACT_APP_CHAIN_ID]}`
+              : `https://dex.zum.finance/#/add/${liquidityUrlPathParts}`
           }
         >
           {lpLabel}
